0570f54b4a6e97a0138e68305994057f9ecb773e
pasinsiri <pasin_plp0206@hotmail.com>
2025-01-25 23:32:13 +0700

    set backup file

--- db_connector.py
+++ db_connector.py
@@ -7,3 +7,14 @@ import requests
 db_url = "https://storage.googleapis.com/benchmarks-artifacts/travel-db/travel2.sqlite"
 local_file = "travel2.sqlite"
 
+# The backup lets us restart for each tutorial section
+backup_file = "travel2.backup.sqlite"
+overwrite = False
+if overwrite or not os.path.exists(local_file):
+    response = requests.get(db_url)
+    response.raise_for_status()  # Ensure the request was successful
+    with open(local_file, "wb") as f:
+        f.write(response.content)
+    # Backup - we will use this to "reset" our DB in each section
+    shutil.copy(local_file, backup_file)
+
