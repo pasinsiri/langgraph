898f8a5b9b8350150f1705dc9115ffcf56478a41
pasinsiri <pasin_plp0206@hotmail.com>
2025-01-26 20:44:44 +0700

    create a GET request

--- tools.py
+++ tools.py
@@ -4,3 +4,7 @@ import numpy as np
 import openai
 from langchain_core.tools import tool
 
+response = requests.get(
+    "https://storage.googleapis.com/benchmarks-artifacts/travel-db/swiss_faq.md"
+)
+response.raise_for_status()
