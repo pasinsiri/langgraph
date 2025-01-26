f84027e50be80ddd572606b810ec2d9e14590dec
pasinsiri <pasin_plp0206@hotmail.com>
2025-01-26 20:45:35 +0700

    get content from response

--- tools.py
+++ tools.py
@@ -8,3 +8,5 @@ response = requests.get(
     "https://storage.googleapis.com/benchmarks-artifacts/travel-db/swiss_faq.md"
 )
 response.raise_for_status()
+faq_text = response.text
+docs = [{"page_content": txt} for txt in re.split(r"(?=\n##)", faq_text)]
