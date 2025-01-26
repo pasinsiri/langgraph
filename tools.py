e566d2693a6943edcdde9956840a5b4fb3aa8b5c
pasinsiri <pasin_plp0206@hotmail.com>
2025-01-26 20:45:49 +0700

    initialize VectorStoreRetriever

--- tools.py
+++ tools.py
@@ -10,3 +10,9 @@ response = requests.get(
 response.raise_for_status()
 faq_text = response.text
 docs = [{"page_content": txt} for txt in re.split(r"(?=\n##)", faq_text)]
+
+class VectorStoreRetriever:
+    def __init__(self, docs: list, vectors: list, oai_client):
+        self._arr = np.array(vectors)
+        self._docs = docs
+        self._client = oai_client
